6f59d2b6fb2fc6bdcfee482eff19baa4512c8434
Pascal Mercat <pascal.mercat@gmail.com>
2021-12-01 17:29:29 +0100

    test

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/favourites", async (req, res) => {
   try {
     console.log(req.fields.favTab[0]);
     let fav = [];
-    for (let i = 0; i < req.fields.favTab[0].length; i++) {
+    for (let i = 1; i < req.fields.favTab[0].length; i++) {
       // faire un requete pour récupérer les info d'un char
       const response = await axios.get(
         `${apiUrl}/characters?apiKey=${apiKey}&name=${favTab[0][i]}`
